bbe6071212c4af8ab71c575bb65beca7af1fe915
VedantiLadda <vedantiladda@Vedantis-MacBook-Pro.local>
2018-06-15 09:46:54 +0530

    15/06 9:46

--- app/src/main/java/com/example/vedantiladda/ecommerce/IApiCall.java
+++ app/src/main/java/com/example/vedantiladda/ecommerce/IApiCall.java
@@ -4,8 +4,12 @@ import com.example.vedantiladda.ecommerce.model.Category;
 import com.example.vedantiladda.ecommerce.model.ProductDTO;
 
 import java.util.List;
+
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.Call;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface IApiCall {
@@ -17,5 +21,20 @@ public interface IApiCall {
 
     @GET("/product/getProductsByCategory/{category}" )
     Call<List<ProductDTO>> getAllProducts(@Path("category") String category);
+
+    @GET("/cart/getAllCartItems/{userId}")// we need to fetch the details via user id, we get list of products added in the cart.
+    Call <List<String>> getCartId(@Path("userId") String userId);
+
+
+    @DELETE("/cart/deleteFromCart/{userId}/{productId}")
+    Call <Boolean> removeProductCartId(@Path("userId") String userId,@Path("productId") String productId);
+
+    //add item to te cart need to send user id and product id
+
+    @GET("/cart/addToCart/{userId}/{productId}/{merchantId}")
+    Call <Void> addProduct(@Path("userId") String userId,@Path("productId") String productId,@Path("merchantId") String merchantId);
+
+    @POST("/product/getProductsByIds")
+    Call <List<ProductDTO>> getCartProducts(@Body List<String> stringList);
 
 }
